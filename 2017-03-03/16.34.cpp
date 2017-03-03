2be6444c737753e56900371647398df500d51b5e
l453821056 <lixinrui000@gmail.com>
2017-03-03 14:06:05 +0800

    daily

---
+++ 2017-03-03/16.34.cpp
@@ -0,0 +1,15 @@
+#include <functional>
+#include <iostream>
+using namespace std;
+template <class T> int compare(const T v1, const T v2)
+{
+    if(less<T>() (v1, v2)) return -1;
+    if(less<T>() (v1, v2)) return 1;
+    return 0;
+}
+int main()
+{
+   compare("hi", "world");
+   compare("hi", "OK");
+    return 0;
+}

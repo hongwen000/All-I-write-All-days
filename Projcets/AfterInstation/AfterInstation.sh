12bb7ea154d551b6c4e03833d905a1fd548f9506
hongwen000 <hongwen000@outlook.com>
2017-03-11 11:57:44 +0800

    daily

--- Projcets/AfterInstation/AfterInstation.sh
+++ Projcets/AfterInstation/AfterInstation.sh
@@ -10,6 +10,7 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 #git
 git config --global http.proxy 'socks5://127.0.0.1:1080'
 git config --global https.proxy 'socks5://127.0.0.1:1080'
+git config credential.helper store
 
 #fish
 curl -L https://get.oh-my.fish | fish

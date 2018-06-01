84068814ab48bf8a922db13b437a67be63eafdff
李新锐 <hongwen000@outlook.com>
2018-06-01 09:38:04 +0800

    Diary tool

---
+++ 2018-06-01/diary.py
@@ -0,0 +1,26 @@
+import sys, os
+import arrow
+import os.path
+
+from weather import Weather, Unit
+
+date = arrow.now().format('YYYY-MM-DD')
+path = '/Users/lixinrui/onedrive/Diary/source/_posts/'
+filename = path + date + ".md"
+if os.path.isfile(filename):
+    os.system("open -a Typora " + filename)
+    sys.exit(0)
+
+os.chdir(path)
+os.system("hexo n " + date)
+f = open(filename, 'a')
+
+weather = Weather(unit=Unit.CELSIUS)
+
+lookup = weather.lookup(2161838)
+condition = lookup.condition.text
+high = lookup.forecast[0].high
+low = lookup.forecast[0].low
+
+print("{}, {}° - {}°".format(condition, low, high), file = f)
+os.system("open -a Typora " + filename)
